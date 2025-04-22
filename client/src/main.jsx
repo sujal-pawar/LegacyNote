19d0bc413fc5f42794529cddbde566377519a114
sujal-pawar <sujalpawar00007@gmail.com>
2025-04-22 20:45:38 +0530

    Added speed insights by vercel

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 // Check if AbortController is available and polyfill if needed
 if (typeof window !== 'undefined' && !window.AbortController) {
@@ -42,5 +43,6 @@ if (typeof window !== 'undefined') {
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>,
 )
